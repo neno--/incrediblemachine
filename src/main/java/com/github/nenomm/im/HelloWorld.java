4d70b4a7e3c246496f681ae002421ec2a9153371
neno-- <neno.nenadovic@gmail.com>
2017-03-18 20:30:08 +0100

    add spring context support

--- src/main/java/com/github/nenomm/im/HelloWorld.java
+++ src/main/java/com/github/nenomm/im/HelloWorld.java
@@ -1,13 +1,17 @@
 package com.github.nenomm.im;
 
 import org.joda.time.LocalTime;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class HelloWorld {
-  public static void main(String[] args) {
-    LocalTime currentTime = new LocalTime();
-    System.out.println("The current local time is: " + currentTime);
+	public static void main(String[] args) {
+		LocalTime currentTime = new LocalTime();
+		System.out.println("The current local time is: " + currentTime);
 
-    Greeter greeter = new Greeter();
-    System.out.println(greeter.sayHello());
-  }
+		Greeter greeter = new Greeter();
+		System.out.println(greeter.sayHello());
+
+		ApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "test.xml" });
+	}
 }
