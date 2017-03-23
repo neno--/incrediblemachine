b15d473f86731b4fc1246a3e873a3be73fd41008
neno-- <neno.nenadovic@gmail.com>
2017-03-23 16:20:51 +0100

    implement autowiring and method injections

--- src/main/java/com/github/nenomm/im/Talker.java
+++ src/main/java/com/github/nenomm/im/Talker.java
@@ -1,11 +1,16 @@
 package com.github.nenomm.im;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
 public class Talker implements ApplicationContextAware {
+	static Logger logger = LoggerFactory.getLogger(Greeter.class);
+
 	private ApplicationContext applicationContext;
+	private Greeter greeterByStaticFactory;
 
 	public void doSomething() {
 		Greeter greeter = (Greeter) applicationContext.getBean("greeter");
@@ -16,5 +21,13 @@ public class Talker implements ApplicationContextAware {
 		this.applicationContext = applicationContext;
 	}
 
+	public void setGreeterByStaticFactory(Greeter greeterByStaticFactory) {
+		this.greeterByStaticFactory = greeterByStaticFactory;
+		logger.info("Autowired.");
+	}
 
+	public Greeter createNewGreeterEveryTime() {
+		// this is a lookup method
+		return null;
+	}
 }
