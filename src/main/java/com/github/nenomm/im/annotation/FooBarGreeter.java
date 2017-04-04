65c60ab9df1e6c5fb241bb06c6b88a42c5013cb4
neno-- <neno.nenadovic@gmail.com>
2017-04-04 22:33:30 +0200

    use annotations to create beans

--- src/main/java/com/github/nenomm/im/annotation/FooBarGreeter.java
+++ src/main/java/com/github/nenomm/im/annotation/FooBarGreeter.java
@@ -4,6 +4,8 @@ import javax.annotation.PostConstruct;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,5 +15,19 @@ public class FooBarGreeter {
 	@PostConstruct
 	private void initMethod() {
 		logger.info("Annotation based post contruct for component");
+	}
+
+	// when invoked from @Component, @Beans do not get proxied by CGLIB
+	@Bean
+	public FooGreeter makeFromComponent(@Value("${another.property}") int i) {
+		// if you want to use autowiring, use #{} syntax instead
+		FooGreeter fooGreeter = new FooGreeter(i);
+		return fooGreeter;
+	}
+
+	@Bean
+	public FooGreeter anotherFooGreeter(@Value("${another.property}") int i) {
+		FooGreeter fooGreeter = new FooGreeter(i);
+		return fooGreeter;
 	}
 }
--- src/main/java/com/github/nenomm/im/annotation/FooGreeter.java
+++ src/main/java/com/github/nenomm/im/annotation/FooGreeter.java
@@ -1,5 +1,16 @@
 package com.github.nenomm.im.annotation;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class FooGreeter {
+	static Logger logger = LoggerFactory.getLogger(FooGreeter.class);
+
+	public FooGreeter() {
+	}
+
+	public FooGreeter(int i) {
+		logger.info("Initialized with custom constructor. Value is {}", i);
+	}
 
 }
