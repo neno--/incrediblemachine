6648a044d044dcf32911e5ae8084c0355e61aac7
neno-- <neno.nenadovic@gmail.com>
2017-04-14 09:58:27 +0200

    test bean creation when there is no default constructor

---
+++ src/main/java/com/github/nenomm/im/validation/SimpleGreeter.java
@@ -0,0 +1,8 @@
+package com.github.nenomm.im.validation;
+
+public class SimpleGreeter {
+	public SimpleGreeter(int i) {
+		// there is no default constructor... can spring pull it out?
+		// nope.
+	}
+}
