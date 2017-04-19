350e2d33984a819cdc4daa9f83ad404454a9e263
neno-- <neno.nenadovic@gmail.com>
2017-04-19 12:43:08 +0200

    implement application listeners

---
+++ src/main/java/com/github/nenomm/im/containerextension/listener/ContextEventSnitch.java
@@ -0,0 +1,11 @@
+package com.github.nenomm.im.containerextension.listener;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+
+public class ContextEventSnitch implements ApplicationListener {
+	@Override
+	public void onApplicationEvent(ApplicationEvent event) {
+		return;
+	}
+}
