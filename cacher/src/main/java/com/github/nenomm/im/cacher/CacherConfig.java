1f0e2e5d7baf960be1068624ac560e2d7b408af1
neno-- <neno.nenadovic@gmail.com>
2017-05-29 08:09:14 +0200

    enable caching using concurrent map implementation (non cluster proof)

--- cacher/src/main/java/com/github/nenomm/im/cacher/CacherConfig.java
+++ cacher/src/main/java/com/github/nenomm/im/cacher/CacherConfig.java
@@ -2,6 +2,9 @@ package com.github.nenomm.im.cacher;
 
 import javax.sql.DataSource;
 
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
@@ -11,6 +14,7 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import com.github.nenomm.im.jdbc.simplejdbc.SomeDao;
 
 @Configuration
+@EnableCaching
 public class CacherConfig {
 	@Bean
 	public DataSource dataSource() {
@@ -26,5 +30,10 @@ public class CacherConfig {
 	@Bean
 	public SomeDao someDao() {
 		return new SomeDao();
+	}
+
+	@Bean
+	public CacheManager cacheManager() {
+		return new ConcurrentMapCacheManager();
 	}
 }
--- cacher/src/main/java/com/github/nenomm/im/cacher/service/UserService.java
+++ cacher/src/main/java/com/github/nenomm/im/cacher/service/UserService.java
@@ -1,6 +1,7 @@
 package com.github.nenomm.im.cacher.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import com.github.nenomm.im.jdbc.simplejdbc.SomeDao;
@@ -15,6 +16,7 @@ public class UserService {
 		return someDao.findUser(id);
 	}
 
+	@Cacheable("users")
 	public User findCachedUser(long id) {
 		return someDao.findUser(id);
 	}
